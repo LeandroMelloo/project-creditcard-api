f9e28057acf0631d3d1cb6b6440636efeda518e6
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-08-21 09:23:59 -0300

    Subindo o Admin

--- creditcard/admin.py
+++ creditcard/admin.py
@@ -7,13 +7,14 @@ class CreditCardAdmin(admin.ModelAdmin):
     """
     Classe de administração para o modelo CreditCard.
 
-    Esta classe define as configurações de exibição e pesquisa para o modelo CreditCard
-    no painel de administração do Django.
+    Esta classe define as configurações de exibição e pesquisa
+    para o modelo CreditCard no painel de administração do Django.
 
     Attributes:
         model (CreditCard): O modelo a ser administrado.
-        list_display (tuple): Tupla contendo os campos a serem exibidos na lista de registros do modelo.
-        search_fields (tuple): Tupla contendo os campos pelos quais os registros podem ser pesquisados.
+        list_display (tuple): Tupla contendo os campos a serem exibidos
+        na lista de registros do modelo. search_fields (tuple): Tupla contendo
+        os campos pelos quais os registros podem ser pesquisados.
 
     """
 
